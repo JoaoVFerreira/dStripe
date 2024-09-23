e7dc79de34d217c116da724166b2ebb64c5ec73b
João Ferreira <joaovtf39@gmail.com>
2024-09-23 12:42:04 -0300

    test: add helper functions to be possible testing

--- blockchain/test/Destripe.test.ts
+++ blockchain/test/Destripe.test.ts
@@ -1,20 +1,37 @@
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { time, loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
-import hre from "hardhat";
+import { ethers } from "hardhat";
 
 describe("Destripe", function () {
-  async function deployOneYearLockFixture() {
-    const [owner, otherAccount] = await hre.ethers.getSigners();
-    const Destripe = await hre.ethers.getContractFactory("Destripe");
-    const destripe = await Destripe.deploy()
+  async function deployFixture() {
+    const DestripeCoin = await ethers.getContractFactory("DestripeCoin");
+    const destripeCoin = await DestripeCoin.deploy();
 
-    return { destripe, owner, otherAccount };
+    await destripeCoin.waitForDeployment();
+
+    const DestripeCollection = await ethers.getContractFactory("DestripeCollection");
+    const destripeCollection = await DestripeCollection.deploy();
+
+    await destripeCollection.waitForDeployment();
+    const collectionAddress = await destripeCollection.getAddress();
+
+    const Destripe = await ethers.getContractFactory("Destripe");
+    const coinAddress = await destripeCoin.getAddress();
+    const destripe = await Destripe.deploy(coinAddress, collectionAddress);
+
+    await destripe.waitForDeployment();
+    const destripeAddress = await destripe.getAddress();
+
+    await destripeCollection.setAuthorizedContract(destripeAddress);
+
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    await destripeCoin.mint(otherAccount.address, ethers.parseEther("1"));
+
+    return { destripe, destripeAddress, destripeCoin, coinAddress, destripeCollection, collectionAddress, owner, otherAccount };
   }
 
-  it('', () => {
-
+  it('Should do', async () => {
+    const { destripe, destripeAddress, destripeCoin, otherAccount } = await loadFixture(deployFixture);
   })
 });
